3dd2ebeda31c60cdb63f78ac1bf6628cd054561d
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-19 16:54:23 +0200

    Added state and event handling to ToggleColor component to toggle background and text colors on button click.

--- src/ToggleColor.jsx
+++ src/ToggleColor.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToggleColor() {
-  return <div>ToggleColor</div>;
+  const [backgroundColor, setBackgroundColor] = useState("white");
+  const [textColor, setTextColor] = useState("#1b1b1b");
+  const [buttonStyle, setButtonStyle] = useState("white");
+
+  function handleClick() {
+    setBackgroundColor(backgroundColor === "white" ? "#1b1b1b" : "white");
+    setTextColor(textColor === "#1b1b1b" ? "#ffa31a" : "#1b1b1");
+    setButtonStyle(backgroundColor === "white" ? "#11b1b1b" : "white");
+  }
+
+  return (
+    <>
+      <section
+        style={{
+          backgroundColor,
+          color: textColor,
+        }}
+      >
+        <button
+          onClick={handleClick}
+          style={{
+            buttonStyle,
+            color: textColor,
+            border: `2px solid ${textColor}`,
+          }}
+        >
+          Wite Theme
+        </button>
+      </section>
+
+      <section className="content">
+        <h1>
+          Welcome To A<br />
+          Real World
+        </h1>
+      </section>
+    </>
+  );
 }
 
 export default ToggleColor;
